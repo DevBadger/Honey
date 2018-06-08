0dc2b52b5e54f4ce01f9800444ba994e6856a73c
Badger <mike@devbadger.com>
2018-06-08 16:56:51 -0600

    Init

---
+++ discovery/src/main/java/com/devbadger/DiscoveryApp.java
@@ -0,0 +1,16 @@
+package com.devbadger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by Badger on 6/8/18.
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class DiscoveryApp {
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoveryApp.class, args);
+    }
+}
